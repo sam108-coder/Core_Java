535e08a7b05c6ce1332f9afebc88f233d42436eb
sammy Kumar <sammykumardemo@gmail.com>
2025-01-15 15:15:45 +0530

    added new files

--- CHAPTER_10_JAVA8/src/flatMapAndMap/MapAndFlatMapExample.java
+++ CHAPTER_10_JAVA8/src/flatMapAndMap/MapAndFlatMapExample.java
@@ -1,4 +1,28 @@
 package flatMapAndMap;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MapAndFlatMapExample {
+    public static void main(String[] args) {
+        List<String> sentences = Arrays.asList("Hello World", "Java Streams", "FlatMap Example");
+
+        //using map
+        List<String[]> wordsArray = sentences.stream()
+                .map(sentence -> sentence.split(" "))
+                .collect(Collectors.toList());
+        System.out.println(wordsArray);
+
+        List<String> upperCaseWords = sentences.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        System.out.println(upperCaseWords);
+
+        // using flatMap
+        List<String> words = sentences.stream()
+                .flatMap(sentence -> Arrays.stream(sentence.split(" ")))
+                .collect(Collectors.toList());
+        System.out.println(words);
+    }
 }
--- CHAPTER_9_COLLECTION/src/mapInterface/concurrentHashMap.java
+++ CHAPTER_9_COLLECTION/src/mapInterface/concurrentHashMap.java
@@ -1,4 +1,37 @@
 package mapInterface;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class concurrentHashMap {
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+
+        System.out.println("Initial map:" + map);
+
+        Thread t1 = new Thread(() -> {
+            map.put("D", 4);
+            System.out.println("Thread 1 added D: " + map);
+        });
+        Thread t2 = new Thread(() -> {
+            map.put("E", 5);
+            System.out.println("Thread 2 added E: " + map);
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Final map: " + map);
+
+    }
 }
